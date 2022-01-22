f8e5345a742c7b7b65f063298b346fb2fedc63cd
Tam Le <tamhle824@gmail.com>
2022-01-21 20:01:30 -0600

    update file

---
+++ blind_75/java/TwoSum.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target){
+        int[] arr = new int[2];
+        for (int i = 0;i < nums.length;i++) {
+            int temp = target - nums[i];
+            arr[0] = i;
+            for (int j = 1; j < nums.length; j++) {
+                if (temp == nums[j]) {
+                    arr[1] = j;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public void main(String[] args) {
+        int[] ar1 = {3,3};
+        System.out.println(twoSum(ar1,6));
+    }
+}
