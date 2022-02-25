b14f78f299eb24d724d79c0fbdebd1be4137463c
Tam Le <tamhle824@gmail.com>
2022-02-24 23:07:19 -0600

    longest palindrome

--- blind_75/java/LongestPalindrome.java
+++ blind_75/java/LongestPalindrome.java
@@ -1,7 +1,20 @@
+
 import java.util.*;
 
 public class LongestPalindrome{
     public static void main(String[] args){
-        
+
+        String s = "babad";
+
+        HashMap<Character,Integer> map = new HashMap<Character,Integer>();
+
+        for(int i =0;i< s.length();i++){
+            s.charAt(i);
+        }
+    
+
+
+
+
     }
 }
