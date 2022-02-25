9ed2ba756f1170566cc1415cb7f130cba6b16b5c
Tam Le <tamhle824@gmail.com>
2022-02-24 22:16:18 -0600

    working on longestSubstring

--- blind_75/java/LongestSubstring.java
+++ blind_75/java/LongestSubstring.java
@@ -1,3 +1,33 @@
+import java.util.*;
+
 public class LongestSubstring {
-    
+
+
+    public static void main(String[] args) {
+        HashMap<Character,Integer> map = new HashMap<Character,Integer>();
+        int max_size = 1;
+
+        String s = "dvdf";
+        
+        if(s.length()==0){
+            max_size = 0;
+        }
+        
+        for(int i =0;i < s.length(); i++){
+            
+            if(!map.containsKey(s.charAt(i))){
+                map.put(s.charAt(i),i);
+            }else{
+                i = map.get(s.charAt(i))+1;
+                map.clear();
+            }
+            if(max_size < map.size()){
+                max_size = map.size();
+            }
+          
+        }
+        
+
+        System.out.println(max_size);
+    }
 }
